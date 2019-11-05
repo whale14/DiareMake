bc488dcf0772a61976dc3b90e242d2efa97e071f
Ideal Idiot <zzazzu14@gmail.com>
2019-11-05 14:20:21 +0900

    짜잔

--- app/src/main/java/com/example/diaremake/MainActivity.java
+++ app/src/main/java/com/example/diaremake/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.diaremake;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
@@ -8,6 +9,9 @@ import android.os.Bundle;
 import android.view.View;
 
 import com.example.diaremake.databinding.ActivityMainBinding;
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,12 +21,29 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
 
-        binding.writeBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(getApplicationContext(), WriteActivity.class));
-            }
-        });
-
+        binding.writeBtn.setOnClickListener(v);
+        binding.logoutBtn.setOnClickListener(v);
     }
+
+    View.OnClickListener v = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.write_btn:
+                    startActivity(new Intent(getApplicationContext(), WriteActivity.class));
+                    break;
+                case R.id.logout_btn:
+                    AuthUI.getInstance()
+                            .signOut(getApplicationContext())
+                            .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    // ...
+                                }
+                            });
+                    startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+                    finish();
+                    break;
+            }
+        }
+    };
 }
--- app/src/main/java/com/example/diaremake/WriteActivity.java
+++ app/src/main/java/com/example/diaremake/WriteActivity.java
@@ -1,10 +1,16 @@
 package com.example.diaremake;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
+import com.example.diaremake.databinding.ActivityWriteBinding;
 import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.text.DateFormat;
 
 public class WriteActivity extends AppCompatActivity {
 
@@ -14,5 +20,15 @@ public class WriteActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_write);
+        ActivityWriteBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_write);
+
+        binding.squareImage.setOnClickListener(v);
     }
+
+    View.OnClickListener v = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Log.d("1", "onClick: 1");
+        }
+    };
 }
